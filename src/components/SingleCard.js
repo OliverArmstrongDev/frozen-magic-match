afd5dd7faaec26b47c4b6f236d60fe5438ca4a8a
Ollie <osarmstrong@gmail.com>
2022-04-04 20:04:07 +1200

    rewrite for context state

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,18 +1,27 @@
+import { useContext } from "react";
+import { MainContext } from "../contexts/GeneralContext";
 import "./SingleCard.css"
+
 
 const cover = "./img/cover.jpg";
 
+
 export default function SingleCard({card, handleChoice, flipped, disabled}) {
 
+  const {state} = useContext(MainContext);
+
+ 
     const handleClick = () => {
        if(!disabled) {
            handleChoice(card);
+           console.log('dis', disabled);
+           
         } 
     }
-
+    
   return (
     <div className= "card">
-        <div className={flipped ? "flipped": ""}>
+        <div className={flipped ? "flipped" : ""}>
             <img className= "front" src={card.src} alt="front" />
             <img 
             onClick={handleClick} 
